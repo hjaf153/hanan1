4c8e477e316d36088fb0c552b5c9b9f0e6d134ed
hjaf153 <hjaf153@gmail.com>
2026-05-09 23:06:59 +0300

    Add files via upload

---
+++ coneection1.java
@@ -0,0 +1,14 @@
+package Library;
+
+import java.sql.*;
+import java.util.logging.*;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+public class coneection1 extends JFrame {
+      
+   Connection con = null;
+   PreparedStatement pst = null;
+   ResultSet rs = null;
+
+}
